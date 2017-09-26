2311233eecae343b6d4acb67b1fa483781672d7b
Ben Matase <bdm015@bucknell.edu>
2017-09-25 22:16:05 -0400

    Focused on Iraq

--- persuasive/script.js
+++ persuasive/script.js
@@ -28,7 +28,6 @@ var options = {
   maptype: 'satellite' //https://developers.google.com/maps/documentation/javascript/maptypes
 }
 
-var colors = ["#fff7fb", "#ece7f2", "#d0d1e6", "#a6bddb", "#74a9cf", "#3690c0", "#0570b0", "#045a8d", "#023858"];
 
 var countryHashMissMap = {"United States":"United States of America", "Tanzania":"United Republic of Tanzania", "Palestinian Territory, Occupied": "West Bank"}
 
@@ -48,11 +47,13 @@ var currDate = new Date();
 var endDate = new Date(2015, 8, 28);
 var largestDeaths = 20000;
 
+
 function preload(){
   img = loadImage(myMap.imgUrl);
   // A geoJSON file with world coordinates for all countries.
   data = loadJSON('data/world.geojson');
   table = loadTable("data/suicide_attacks.csv", "csv", "header");
+  explosion = loadSound("data/explosion.mp3");
 }
 
 function setup(){
@@ -87,7 +88,7 @@ function setup(){
 
 	initializeCountries(data["features"], countriesAttacked);
 
-	for(var d = new Date(1982, 0, 1); d.toString() !== endDate.toString(); d.setDate(d.getDate() + 1)) {
+	for(var d = new Date(2000, 0, 1); d.toString() !== endDate.toString(); d.setDate(d.getDate() + 1)) {
 		if(!Object.keys(dateToEventMap).includes(d.toString())) {
 			dateToEventMap[d] = [];
 		}
@@ -95,15 +96,26 @@ function setup(){
 
 	console.log(dateToEventMap);
 
-	currDate = new Date(1982, 0, 1);
+	currDate = new Date(2000, 0, 1);
 	console.log(currDate);
-	
+
+	//code to download json file of import objects
+	/*
+	var objToBeSaved = {"countryHashMap": countryHashMap,
+	"dateToEventMap": dateToEventMap};
+	var dataStr = "data:text/json;charset=utf-8," + encodeURIComponent(JSON.stringify(objToBeSaved));
+	var dlAnchorElem = document.getElementById('downloadAnchorElem');
+	dlAnchorElem.setAttribute("href", dataStr);
+	dlAnchorElem.setAttribute("download", "scene.json");
+	dlAnchorElem.click();	
+	*/
 }
 
 
 function draw() {
 	currDate.setDate(currDate.getDate() + 1);
 	attackCountries(dateToEventMap[currDate.toString()]);
+	stroke(111, 111, 111);
 	redrawCountries();
 	console.log(currDate);
 
@@ -121,6 +133,11 @@ function initializeCountries(countries, countriesAttacked) {
 }
 
 function attackCountries(countries) {
+	var countryNames = countries.map(function(value) { return value[0]; });
+	if(countryNames.includes("Iraq")) {
+		explosion.play();
+	}
+
 	for(var i = 0; i < countries.length; i ++) {
 		var countryName = countries[i][0];
 		var numberKilled = countries[i][1];
@@ -148,12 +165,15 @@ function redrawCountries() {
 
 function drawCountry(countryName) {
 	var country = countryHashMap[countryName];
-	var fillColor = country['properties']['currentcolor'];
-	
+	var currColor = country['properties']['currentcolor'];
+	var fillColor = country['properties']['name'] === "Iraq" ? 
+		color(currColor, 0, 0):
+		color(currColor, currColor, 0);
+
 	if(country['geometry']['type'] === 'Polygon') {
 		polygon = country['geometry']['coordinates'];
 		beginShape();
-		fill(fillColor, 0, 0);
+		fill(fillColor);
 		for (var j = 0; j < polygon[0].length; j ++){
 			var pos = myMap.latLngToPixel(polygon[0][j][1], polygon[0][j][0]);
 			vertex(pos.x, pos.y);
@@ -163,7 +183,7 @@ function drawCountry(countryName) {
 		multiPolygon = country['geometry']['coordinates'];
 		for(var k = 0; k < multiPolygon.length; k++){
 			beginShape();
-			fill(fillColor, 0, 0);
+			fill(fillColor);
 			for (var j = 0; j < multiPolygon[k][0].length; j ++){
 				var pos = myMap.latLngToPixel(multiPolygon[k][0][j][1], multiPolygon[k][0][j][0]);
 				vertex(pos.x, pos.y);
@@ -174,8 +194,7 @@ function drawCountry(countryName) {
 }
 
 function scaleRed(deaths) {
-	var scaledVal = 255 * deaths / largestDeaths;
-	return scaledVal;
+	return 255 * deaths / largestDeaths;
 }
 
 function fixCountryName(name) {
